c1c92b039d917680406b5b35045dcd9c8178c3e4
Priyanshu <priyanshusaxena221@gmail.com>
2026-09-16 19:54:46 +0530

    changed timeout

--- frontend/client/src/services/api.ts
+++ frontend/client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
-    timeout: 12_000,
+    timeout: 60_000,
 })
 
 export default api;
