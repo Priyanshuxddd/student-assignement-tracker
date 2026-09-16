c117742208852e3f3ffca6ebee11276368b220c7
Priyanshu <priyanshusaxena221@gmail.com>
2026-09-16 16:58:26 +0530

    Done with all ui tweaks

--- frontend/client/src/components/AssignmentCard.tsx
+++ frontend/client/src/components/AssignmentCard.tsx
@@ -1,43 +1,28 @@
 interface Assignment {
-    id: number;
-    title: string;
-    description: string | null;
-    dueDate: string;
-    completed: boolean;
-  }
-  
-  interface AssignmentCardProps {
-    assignment: Assignment;
-    onDelete : (id:number) => void;
-    onEdit : (assignment: Assignment) => void;
-  }
-
-
-
-  const AssignmentCard  = ( {assignment, onDelete, onEdit } : AssignmentCardProps) => {
-
-    console.log(assignment);
-    
-    return (
-
-<div className="card">
-    
-    <h2>{assignment.title}</h2>
-
-    <p>{assignment.description}</p>
-
-    <p>{new Date(assignment.dueDate).toLocaleDateString()}</p>
-
-    <span className="status">{assignment.completed ? "Completed" : "Pending"}</span>
-
-    <div className="actions">
-        <button onClick={()=> onEdit(assignment)} style={{color:"yellow"}}>Edit</button>
-        <button onClick={()=> onDelete(assignment.id)} style={{color:"red"}}>Delete</button>
-    </div>
-
-    </div>
-
-    )
+  id: number
+  title: string
+  description: string | null
+  dueDate: string
+  completed: boolean
 }
 
-export default AssignmentCard;
+interface AssignmentCardProps {
+  assignment: Assignment
+  onDelete: (id: number) => void
+  onEdit: (assignment: Assignment) => void
+}
+
+const AssignmentCard = ({ assignment, onDelete, onEdit }: AssignmentCardProps) => {
+  return <div className="card">
+    <h2>{assignment.title}</h2>
+    <p>{assignment.description}</p>
+    <p>{new Date(assignment.dueDate).toLocaleDateString()}</p>
+    <span className={`status ${assignment.completed ? "completed" : ""}`}>{assignment.completed ? "Completed" : "Pending"}</span>
+    <div className="actions">
+      <button className="edit-button" onClick={() => onEdit(assignment)}>Edit</button>
+      <button className="delete-button" onClick={() => onDelete(assignment.id)}>Delete</button>
+    </div>
+  </div>
+}
+
+export default AssignmentCard
